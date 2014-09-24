035de379f3c79764d4df1454b5f0091707651017
doc <doc.divxm@gmail.com>
2014-09-25 01:09:30 +0300

    flo: Remove f2fs tools
    ** They are in vendor/aicp/configs/common.mk already
    
    Change-Id: I6da4815dd28df69f1c3ee06ea97790518b6cbe01

--- device.mk
+++ device.mk
@@ -33,13 +33,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     nfc_nci.flo
 
-# F2FS filesystem
-PRODUCT_PACKAGES += \
-    mkfs.f2fs \
-    fsck.f2fs \
-    fibmap.f2fs \
-    f2fstat
-
 # the actual meat of the device-specific product definition
 $(call inherit-product, device/asus/flo/device-common.mk)
 

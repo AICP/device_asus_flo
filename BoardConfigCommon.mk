b4b437be77da0dac612a74e0e6e5a609873412fe
doc <doc.divxm@gmail.com>
2015-02-16 22:01:51 +0200

    flo: Add CMHW Tunables
    ** Wake Gestures
    
    Change-Id: Id6812297d47b1b23e89333a41ee6f940790d4884

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,6 +141,7 @@ BOARD_SEPOLICY_UNION += \
         rmt.te \
         sensors.te \
         surfaceflinger.te \
+        system_app.te \
         system_server.te \
         tee.te \
         te_macros \
@@ -152,5 +153,8 @@ USE_MINIKIN := true
 
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
+
+# CMHW
+BOARD_HARDWARE_CLASS := device/asus/flo/cmhw
 
 -include vendor/asus/flo/BoardConfigVendor.mk

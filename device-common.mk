a3bcdaa38de67a95a12bdbc0ff3887544934ebe5
Thierry Strudel <tstrudel@google.com>
2015-07-17 04:30:00 +0300

    flo: force dex2oat not to use swap file
    Change-Id: Icd8c2305b5d374b309c33aca7987a0035fba842f

--- device-common.mk
+++ device-common.mk
@@ -136,7 +136,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-flags=--no-watch-dog
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
+    dalvik.vm.dex2oat-swap=false
 
 # Camera Configuration
 PRODUCT_PROPERTY_OVERRIDES += \

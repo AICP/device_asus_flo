bbf4450e97f27e67b608551ddd3f6541c7078ab4
Zipsnet <hcolmenares@gmail.com>
2018-04-11 12:08:14 -0400

    flo: Fix vendor's mismatch
    Change-Id: I2f98e53f2091b0caf91603c732ff6bb62b0c575a

--- aicp.mk
+++ aicp.mk
@@ -30,8 +30,9 @@ PRODUCT_MANUFACTURER := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=razor \
-    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys \
     PRIVATE_BUILD_DESC="razor-user 6.0.1 MOB30X 3036618 release-keys"
+
+    BUILD_FINGERPRINT := google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
